46349f4f41af734627bb70ded0b68033fa5cd0d0
sunkarisanthosh <77742585+sunkarisanthosh@users.noreply.github.com>
2022-10-13 08:33:49 +0100

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "startup-script" {
-  name     = "${var.project}-startup-script"
-  location = "${var.location}"
-  project  = "${var.project}"
+  name     = "var.name"
+  region = "var.region"
+  project  = "var.project"
 }
