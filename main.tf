e2759d5b910ce5adcf6b1291416d02941070ff51
sunkarisanthosh <77742585+sunkarisanthosh@users.noreply.github.com>
2022-10-13 07:50:52 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+data "google_compute_image" "debian" {
+    family  = "debian-11"
+    project = "debian-cloud"
+}
+
+# Creates a GCP VM Instance.  Metadata Startup script install the Nginx webserver.
+resource "google_compute_instance" "vm" {
+  name         = var.name
+  machine_type = var.machine_type
+  zone         = var.zone
+  tags         = ["http-server"]
+  labels       = var.labels
+
+  boot_disk {
+    initialize_params {
+      image = data.google_compute_image.debian.self_link
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+ metadata_startup_script = "${file("script.txt")}"
+
+ 
+}
